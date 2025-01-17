4433a2d8cfbee3b068f660033cf1c39f0c018a7b
Anthonii Veremko <anton.veremko@gmail.com>
2025-01-17 17:13:35 +0100

    UI shows slider values

--- ui.js
+++ ui.js
@@ -1,43 +1,52 @@
 (function () {
   // Custom progress bars
-  const controls = document.querySelectorAll('.control');
-  controls.forEach((control) => {
-    const progressBarContainer = control.querySelector('.control-range');
-    if (progressBarContainer) {
-      const progress = control.querySelector('.control-range-bar');
-      const uiValue = control.querySelector('.control-value');
+  // const controls = document.querySelectorAll('.control');
+  // controls.forEach((control) => {
+  //   const progressBarContainer = control.querySelector('.control-range');
+  //   if (progressBarContainer) {
+  //     const progress = control.querySelector('.control-range-bar');
+  //     const uiValue = control.querySelector('.control-value');
 
-      // Function to set progress based on click position
-      let clickX = 0;
-      function setProgress(event) {
-        const containerWidth = progressBarContainer.offsetWidth;
-        clickX = event.offsetX; // Position of the click
+  //     // Function to set progress based on click position
+  //     let clickX = 0;
+  //     function setProgress(event) {
+  //       const containerWidth = progressBarContainer.offsetWidth;
+  //       clickX = event.offsetX; // Position of the click
 
-        let newProgress = (clickX / containerWidth) * 100; // Calculate percentage
-        if (newProgress > 100) newProgress = 100;
-        if (newProgress < 0) newProgress = 0;
-        progress.style.width = `${newProgress}%`;
-        console.log(parseInt(newProgress));
-        // update number on UI
-        uiValue.textContent = parseInt(newProgress) + '%';
-      }
-      // Add event listener for dragging
-      let isDragging = false;
-      function draging(event) {
-        if (isDragging) {
-          setProgress(event);
-        }
-      }
+  //       let newProgress = (clickX / containerWidth) * 100; // Calculate percentage
+  //       if (newProgress > 100) newProgress = 100;
+  //       if (newProgress < 0) newProgress = 0;
+  //       progress.style.width = `${newProgress}%`;
+  //       console.log(parseInt(newProgress));
+  //       // update number on UI
+  //       uiValue.textContent = parseInt(newProgress) + '%';
+  //     }
+  //     // Add event listener for dragging
+  //     let isDragging = false;
+  //     function draging(event) {
+  //       if (isDragging) {
+  //         setProgress(event);
+  //       }
+  //     }
 
-      progressBarContainer.addEventListener(
-        'mousedown',
-        () => (isDragging = true)
-      );
-      document.addEventListener('mouseup', () => (isDragging = false));
-      document.addEventListener('mousemove', (event) => draging(event));
-    }
+  //     progressBarContainer.addEventListener(
+  //       'mousedown',
+  //       () => (isDragging = true)
+  //     );
+  //     document.addEventListener('mouseup', () => (isDragging = false));
+  //     document.addEventListener('mousemove', (event) => draging(event));
+  //   }
+  // });
+
+  // progress bars
+  const sliders = document.querySelectorAll('input[type="range"]');
+  sliders.forEach((slider) => {
+    const valueScreen = slider.parentElement.querySelector('.control-value');
+    valueScreen.textContent = slider.value;
+    slider.addEventListener('input', function () {
+      valueScreen.textContent = slider.value;
+    });
   });
-
   // background color buttons
   const backgroundBtns = document.querySelectorAll('.buttons');
   backgroundBtns.forEach((group) => {
